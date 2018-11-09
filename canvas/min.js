0585ac11c49b7314436b29c246de79c36f279956
Chiu Yung <chiuyung@ChiudeiMac.local>
2018-11-09 16:20:14 +0800

    canvas画图板

--- canvas/min.js
+++ canvas/min.js
@@ -5,13 +5,26 @@ var isClear = false //是否是清除
 
 autoCanvasWH()
 
-drawLine()
+cheackDevice()
 
 clearLine()
 
-//描绘线条
-function drawLine() {
-   
+clear.onclick = function(){
+    context.clearRect(0,0,canvas.width,canvas.height)
+}
+
+
+//检测设备是否具有触摸touch事件
+function cheackDevice(){
+    if(document.documentElement.ontouchstart === undefined){//pc端
+        drawPCLine()
+    }else{
+        drawMobileLine()
+    }
+}
+
+//PC描绘线条
+function drawPCLine() {
 
     var isDown = false //是否按下去
     var frame = {
@@ -55,14 +68,59 @@ function drawLine() {
 
 }
 
+//移动端描绘线条
+function drawMobileLine() {
+
+    var isDown = false //是否按下去
+    var frame = {
+        x: undefined,
+        y: undefined
+    }
+
+    document.ontouchstart = function (ele) {
+        isDown = true
+        let X = ele.touches[0].clientX
+        let Y = ele.touches[0].clientY
+        frame = {
+            x: X,
+            y: Y
+        }
+    }
+
+    document.ontouchmove = function (ele) {
+        if (isDown) {
+            let X = ele.touches[0].clientX
+            let Y = ele.touches[0].clientY
+            if (isClear) {
+                context.clearRect(X-25, Y-25, 50, 50)
+            } else {
+                context.beginPath()
+                context.lineWidth = 3
+                context.moveTo(X, Y)
+                context.lineTo(frame.x, frame.y)
+                context.stroke();
+                frame = {
+                    x: X,
+                    y: Y
+                }
+            }
+        }
+    }
+
+    document.ontouchend = function () {
+        isDown = false
+    }
+}
+
+
 function clearLine() {
-    let clearButton = document.getElementById('clear')
-    clearButton.onclick = function () {
+    let eraserButton = document.getElementById('eraser')
+    eraserButton.onclick = function () {
         isClear = true
     }
 
-    let drawButton = document.getElementById('draw')
-    drawButton.onclick = function () {
+    let penButton = document.getElementById('pen')
+    penButton.onclick = function () {
         isClear = false
     }
 }
