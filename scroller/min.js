8e8b197f6766c91fa5823d449bbfa36853b7cf0f
Chiu Yung <chiuyung@ChiudeiMac.local>
2018-11-26 10:45:38 +0800

    no message

--- scroller/min.js
+++ scroller/min.js
@@ -1,17 +1,51 @@
 
 var buttons = $('.window>div>span')
+
 let banner = $('.window>.bannerWrapper')[0]
+
+let wrapper = $('.window>.bannerWrapper')
+
 var index = 0
+
+var timer = autoScroller()
+
+
+$('.window>.bannerWrapper').css({
+    'width' : buttons.length*300+'px'
+})
+
 for (let i = 0; i < buttons.length; i++) {
     $(buttons[i]).on('click',function(){
         index = $(this).index()
-        scroller()
+        selctIndex(index)
+        scroller(index)
     })
 }
 
-function scroller() {
-    scrollerW = index*(-300)
+function scroller(currentIndex) {
+    scrollerW = currentIndex*(-300)
     $(banner).css({
         'transform' : 'translateX('+scrollerW+'px)'
     })
 }
+
+
+function autoScroller(){
+    let time = setInterval(()=>{
+        index++
+        selctIndex(index%(buttons.length))
+        scroller(index%(buttons.length))
+    },2000)
+    return time
+}
+
+function selctIndex(n){
+    buttons.eq(n).addClass('red').siblings('.red').removeClass('red')
+}
+
+wrapper.on('mouseenter',()=>{
+    window.clearInterval(timer)
+})
+wrapper.on('mouseleave',()=>{
+    timer = autoScroller()
+})
