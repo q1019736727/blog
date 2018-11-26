af3a7cf11fdb3b38fda33db6f7305c5002fded9d
Chiu Yung <chiuyung@ChiudeiMac.local>
2018-11-26 17:42:29 +0800

    no message

---
+++ slider/min.js
@@ -0,0 +1,8 @@
+
+var imgs = $('.wind>.images>img')
+// $('.wind>.images').css({width:imgs.length*500+'px'})
+$('.wind>.images>img:nth-child(1)').addClass('enter').siblings().addClass('stac')
+
+setTimeout(()=>{
+
+},3000)
