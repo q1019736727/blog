f79326d91b3f085497ccd0e7c34e336665df29fa
邱勇 <yq@yyqdeMacBook-Pro.local>
2018-11-26 23:24:39 +0800

    no message

--- slider/min.js
+++ slider/min.js
@@ -1,8 +1,34 @@
 
 var imgs = $('.wind>.images>img')
-// $('.wind>.images').css({width:imgs.length*500+'px'})
-$('.wind>.images>img:nth-child(1)').addClass('enter').siblings().addClass('stac')
 
-setTimeout(()=>{
+// $('.wind>.images>img:nth-child(1)').addClass('enter').siblings().addClass('stac')
 
+var n = 1
+
+setInterval(()=>{
+    n = n%3
+    if (n === 0){
+        n = 3
+    }
+    enter($(`.wind>.images>img:nth-child(${n})`))
+        .one('transitionend',(ele)=>{
+            leave($(ele.currentTarget))
+        })
+    stac($(`.wind>.images>img:nth-child(${n+1})`))
+    n++
 },3000)
+
+function enter($ele) {
+    $ele.removeClass('leave stac').addClass('enter')
+    return $ele
+}
+
+function leave($ele) {
+    $ele.removeClass('enter stac').addClass('leave')
+    return $ele
+}
+
+function stac($ele) {
+    $ele.removeClass('enter leave').addClass('stac')
+    return $ele
+}
