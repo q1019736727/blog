c9d0bce42a601437f952424ad64ded0fb806896c
Chiu Yung <chiuyung@ChiudeiMac.local>
2018-11-27 09:51:35 +0800

    no message

--- slider/min.js
+++ slider/min.js
@@ -1,34 +1,37 @@
 
-var imgs = $('.wind>.images>img')
 
-// $('.wind>.images>img:nth-child(1)').addClass('enter').siblings().addClass('stac')
+$('.wind>.images>img:nth-child(1)').addClass('enter').siblings().addClass('stac')
 
 var n = 1
 
 setInterval(()=>{
-    n = n%3
-    if (n === 0){
-        n = 3
-    }
-    enter($(`.wind>.images>img:nth-child(${n})`))
+    leave($(`.wind>.images>img:nth-child(${calculate(n)})`))
         .one('transitionend',(ele)=>{
-            leave($(ele.currentTarget))
+            stac($(ele.currentTarget))
         })
-    stac($(`.wind>.images>img:nth-child(${n+1})`))
+    enter($(`.wind>.images>img:nth-child(${calculate(n+1)})`))
     n++
 },3000)
 
-function enter($ele) {
-    $ele.removeClass('leave stac').addClass('enter')
-    return $ele
-}
-
 function leave($ele) {
-    $ele.removeClass('enter stac').addClass('leave')
+    $ele.removeClass('enter').addClass('leave')
     return $ele
 }
 
 function stac($ele) {
-    $ele.removeClass('enter leave').addClass('stac')
+    $ele.removeClass('leave').addClass('stac')
     return $ele
+}
+
+function enter($ele) {
+    $ele.removeClass('stac').addClass('enter')
+    return $ele
+}
+
+function calculate(n){
+        n = n%4
+        if (n===0){
+            n =4
+        }
+    return n
 }
